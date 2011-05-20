e7b17008f4181e6873b0617b7d13c61924af3a59
Keith Wyss <WyssMan@gmail.com>
2011-05-20 18:03:12 -0400

    Layout adapted a little.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,22 @@
 class PagesController < ApplicationController
   def about
+    @title = "About Us"
   end
 
   def contact
+    @title = "Contact Us"
   end
 
   def bios
+    @title = "The Artists"
   end
 
   def gallery
+    @title = "The Artwork"
   end
 
   def events
+    @title = "Events"
   end
 
 end
