6522de9a2852ec6587e4700b8a8318535d174c37
Keith Wyss <WyssMan@gmail.com>
2011-05-20 18:31:30 -0400

    Got the images working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,27 @@
 class PagesController < ApplicationController
   def about
     @title = "About Us"
+    @imgtag = "about.jpg"
   end
 
   def contact
     @title = "Contact Us"
+    @imgtag = "contact.jpg"
   end
 
   def bios
     @title = "The Artists"
+    @imgtag = "bios.jpg"
   end
 
   def gallery
     @title = "The Artwork"
+    @imgtag = "gallery.jpg"
   end
 
   def events
     @title = "Events"
+    @imgtag = "events.jpg"
   end
 
 end
