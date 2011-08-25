0c2f8ad95f9e18c3b912cdc43aed1c668742230c
Keith Wyss <WyssMan@gmail.com>
2011-08-24 23:46:37 -0400

    Need to test internet explorer, but otherwise, no issues.

--- public/javascripts/trigger.js
+++ public/javascripts/trigger.js
@@ -1,11 +1,10 @@
 //functions to trigger CSS animations to create a menu.
-
 function growit(){
-var elem = document.getElementById("mouseexpand");
-    elem.style.MozDuration = "2000ms";
-  elem.style.WebkitDuration = "2000ms";
-    elem.style.WebkitAnimationName = "growlist";
-    elem.style.MozAnimationName = "growlist";
+	var elem = document.getElementById("mouseexpand");
+	elem.style.WebkitAnimationName = "growlist";
+	elem.style.MozAnimationName = "growlist";
+	elem.style.OTransform = "scale(1,1)";
+
 }
 
 function shrinkit(){
@@ -16,7 +15,18 @@ var elem = document.getElementById('mouseexpand');
     elem.style.WebkitAnimationName = 'shrinklist';
     elem.style.MozAnimationDuration = '5000ms';
     elem.style.MozAnimationName = 'shrinklist';
-
+	elem.style.OTransform = "scale(1,0)";
 
 
 }
+
+function growitopera(){
+	var elem = document.getElementById("mouseexpand");
+	elem.style.OTransform = "scale(1,1)";
+}
+
+function shrinkitopera(){
+var elem = document.getElementById('mouseexpand');
+	elem.style.OTransform = "scale(1,0)";
+}
+
